92cc35ba928f70fa461aa3ee123932646fe82aa6
sunnyashlesh16 <85336488+sunnyashlesh16@users.noreply.github.com>
2025-01-10 01:59:42 -0500

    Update taskServices.js

--- Application-Code/frontend/src/services/taskServices.js
+++ Application-Code/frontend/src/services/taskServices.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const apiUrl = "http://k8s-threetie-mainlb-11c5700e30-116301760.us-east-1.elb.amazonaws.com/api/tasks"; //"http://localhost:8080/api/tasks";
+const apiUrl = process.env.SOMETHING; //"http://localhost:8080/api/tasks";
 console.log(apiUrl)
 export function getTasks() {
     return axios.get(apiUrl);
